f076f5f947e1bed7cb6239c64b1956f130e9b63c
K <kas.bihari@gmail.com>
2025-06-06 13:53:12 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+console.log('Main loaded');
+
+function getRandomInt(max) {
+    return Math.round(Math.random() * max);
+  }
+  const myNumber = getRandomInt(9);
+  console.log(myNumber)
+
+
+function ExampleFunction() {
+    console.log("Hoi, dit is een voorbeeld");
+}
